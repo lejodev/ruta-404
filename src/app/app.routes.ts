81afb7b35cf9654fe6079374f4f6c2d6a5389bf7
Alejandro <alejandro.rv97@gmail.com>
2025-10-12 20:31:08 -0500

    FIX: clean route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './modules/home/components/home/home';
 
-export const routes: Routes = [{
-    path: '', redirectTo: 'home', pathMatch: 'full',
-
-},
-{
-    path: 'home', component: Home
-
-}];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: '**', redirectTo: '' }
+];
