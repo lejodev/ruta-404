7a47b410639f0d3d9c6f8734f0142621cf144c49
Alejandro <alejandro.rv97@gmail.com>
2025-10-12 20:13:51 -0500

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './modules/home/components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '', redirectTo: 'home', pathMatch: 'full',
+
+},
+{
+    path: 'home', component: Home
+
+}];
---
+++ src/app/shared/material/material.imports.ts
@@ -0,0 +1,7 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card'
+
+export const SHARED_MATERIAL_IMPORTS = [
+    MatButtonModule,
+    MatCardModule
+]
