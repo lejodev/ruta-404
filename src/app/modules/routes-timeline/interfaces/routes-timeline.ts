8e3a757c72ed4ef2cc453c9951db71c612c27115
Alejandro <alejandro.rv97@gmail.com>
2025-10-16 10:14:48 -0500

    ADD: Styling refined and timeline section finished

--- src/app/modules/routes-timeline/interfaces/routes-timeline.ts
+++ src/app/modules/routes-timeline/interfaces/routes-timeline.ts
@@ -1,10 +1,12 @@
+import { Photo } from "./photo.interface";
+
 export interface IRoutesTimeline {
     id: number,
     name: string,
     destination: string,
     description?: string,
     distance?: number,
-    photos?: string[],
+    photos?: Photo[],
     youtube_link?: string,
     in_progress?: false,
 }
--- src/app/modules/routes-timeline/mocks/routes.mock.ts
+++ src/app/modules/routes-timeline/mocks/routes.mock.ts
@@ -6,29 +6,141 @@ export const routes: IRoutesTimeline[] = [
         destination: 'Amagá',
         name: 'Túneles antiguo ferrocarril de Antioquia',
         description: 'Viaje a Amagá, sus antguos túneles de más de un siglo de existencia',
+        photos: [
+            {
+                id: 1,
+                url: 'amaga (1).jpg'
+            },
+            {
+                id: 2,
+                url: 'amaga (2).jpg'
+            },
+            {
+                id: 3,
+                url: 'amaga (3).jpg'
+            },
+            {
+                id: 4,
+                url: 'amaga (4).jpg'
+            },
+            {
+                id: 5,
+                url: 'amaga (5).jpg'
+            }
+        ]
+
     },
     {
         id: 2,
         destination: 'Armenia Mantequilla - Heliconia',
         name: 'Rodada corta a Armenia Mantequilla',
-        description: 'Rodada a Armenia Mantequilla,'
+        description: 'Rodada a Armenia Mantequilla,',
+        photos: [
+            {
+                id: 1,
+                url: 'armenia (1).jpg'
+            },
+            {
+                id: 2,
+                url: 'armenia (2).jpg'
+            },
+            {
+                id: 3,
+                url: 'armenia (3).jpg'
+            },
+            {
+                id: 4,
+                url: 'armenia (4).jpg'
+            },
+            {
+                id: 5,
+                url: 'armenia (5).jpg'
+            }
+        ]
     },
     {
         id: 3,
         destination: 'Santa Fe de Antioquia',
         name: 'Tintiando en Santa fe de Antioquia',
-        description: 'Rodada grupal a Santa Fe de Antioquia y San Jerónimo, desayuno y buenas charlas'
+        description: 'Rodada grupal a Santa Fe de Antioquia y San Jerónimo, desayuno y buenas charlas',
+        photos: [
+            {
+                id: 1,
+                url: 'sfe-antioquia (1).jpg'
+            },
+            {
+                id: 2,
+                url: 'sfe-antioquia (2).jpg'
+            },
+            {
+                id: 3,
+                url: 'sfe-antioquia (3).jpg'
+            },
+            {
+                id: 4,
+                url: 'sfe-antioquia (4).jpg'
+            },
+            {
+                id: 5,
+                url: 'sfe-antioquia (5).jpg'
+            }
+        ]
     },
     {
         id: 4,
         destination: 'Concepción - Alejandría',
         name: 'Tres pueblos en una salida',
-        description: 'Rodada con Club Xulszados a Alejandría, Jericó Y remate en Guarne tomando tinto'
+        description: 'Rodada con Club Xulszados a Alejandría, Jericó Y remate en Guarne tomando tinto',
+        photos: [
+            {
+                id: 1,
+                url: 'concepcion (1).jpg'
+            },
+            {
+                id: 2,
+                url: 'concepcion (2).jpg'
+            },
+            {
+                id: 3,
+                url: 'concepcion (3).jpg'
+            },
+            {
+                id: 4,
+                url: 'concepcion (4).jpg'
+            },
+            {
+                id: 5,
+                url: 'concepcion (5).jpg'
+            }
+        ]
     },
     {
         id: 5,
         destination: 'La Unión',
         name: 'Mirador de La Unión',
-        description: 'Viaje orto a la Unión, observación de paisajes majestuosos y que no falte el tinto'
+        description: 'Viaje corto a la Unión, observación de paisajes majestuosos y que no falte el tinto',
+        photos: [
+            {
+                id: 1,
+                url: 'banner.JPG',
+                alt: 'Foto de paisaje en la Union'
+            },
+            {
+                id: 2,
+                url: 'gal- (1).jpg'
+            },
+            {
+                id: 3,
+                url: 'gal- (1).jpg'
+            },
+            {
+                id: 4,
+                url: 'gal- (1).jpg'
+            },
+            {
+                id: 5,
+                url: 'gal- (1).jpg'
+            }
+        ]
     }
 ]
